19408ae6fa61befadb0ad1358457285cf59f0db6
danil <daniil.shmel@list.ru>
2022-01-23 14:19:25 +0400

    add tags instead of classes

--- Bootstrap/clock/clock script.js
+++ Bootstrap/clock/clock script.js
@@ -1,3 +1,11 @@
+const hour_hand = document.getElementById('clock-hour-hand')
+const minute_hand = document.getElementById('clock-minute-hand')
+const second_hand = document.getElementById('clock-second-hand')
+
+const date = new Date()
 function enable_clock() {
-    
+    var rect = second_hand.getBoundingClientRect()
+
+    setTimeout("enable_clock()", 1000);
 }
+
